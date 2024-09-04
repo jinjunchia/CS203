386f97fb32ef10839d569d9be1a7206c127db99b
jinjunchia <144515009+jinjunchia@users.noreply.github.com>
2024-09-04 17:00:11 +0800

    Add files via upload

---
+++ cs203system/src/main/java/com/cs203/cs203system/Cs203systemApplication.java
@@ -0,0 +1,13 @@
+package com.cs203.cs203system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs203systemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs203systemApplication.class, args);
+	}
+
+}
---
+++ cs203system/src/test/java/com/cs203/cs203system/Cs203systemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs203.cs203system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs203systemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
