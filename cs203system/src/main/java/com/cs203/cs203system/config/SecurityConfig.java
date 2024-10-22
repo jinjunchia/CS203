2965038b27f5b35b8c76e2971d625eb756ce9d8c
AlligatorBl00d <arhben18@gmail.com>
2024-10-22 10:28:37 +0800

    builder

--- cs203system/src/main/java/com/cs203/cs203system/config/SecurityConfig.java
+++ cs203system/src/main/java/com/cs203/cs203system/config/SecurityConfig.java
@@ -83,9 +83,7 @@ public class SecurityConfig {
 //                        .requestMatchers("api/auth/register").permitAll()
                         .anyRequest().authenticated()
 
-                )
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        ;
+                );
 
 
         http.httpBasic(withDefaults());
