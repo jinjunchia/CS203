c529951f94f906df3c92279c5c1af02c12088ad8
TheModerateCow <ooibryan988@gmail.com>
2024-11-13 02:58:29 +0800

    undone all changes

--- cs203system/src/test/java/com/cs203/cs203system/Cs203systemApplicationTests.java
+++ cs203system/src/test/java/com/cs203/cs203system/Cs203systemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Cs203systemApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
